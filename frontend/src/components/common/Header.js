98818bc7f08a7df2cdfe5fcf29b7ddcc602ee508
alydemah <alydemah@gmail.com>
2017-12-18 13:03:26 +0100

    adding header

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -1,2 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Link, IndexLink} from 'react-router';
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <IndexLink to="/" activeClassName="active">Home</IndexLink>
+              {" | "}
+              <Link to="/movies" activeClassName="active">Movies</Link>
+               {" | "}
+               <Link to="/about" activeClassName="active">About</Link>
+           </div>
+
+          <div className="collapse navbar-collapse">
+          <ul className="navbar-nav navbar-right">
+          <Link to="/Login" activeClassName="active">About</Link>
+          </ul>
+          </div>
+
+
+        </div>
+      </nav>
+    );
+
+};
+
+
+
+export default Header;
+
